2f8c4dddccc63fefadf86a940f5b95fa691abf69
Aritz Jimenez Garcia <yehuda4klein@gmail.com>
2021-04-23 15:52:28 -0500

    this is the first website
    hello world

---
+++ portfolio.js
@@ -0,0 +1,29 @@
+const GITHUB_URL ="https://api.github.com/users/yehuda4klein" ;
+
+
+fetch(GITHUB_URL)
+.then(function (response) {
+    return response.json();
+})
+
+.then(function (data) {
+    const myName = document.getElementById("my-name");
+    myName.innerText = data.name;
+})
+
+fetch(GITHUB_URL)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    const profileImage = document.getElementById("profile-image");
+    profileImage.src = data.avatar_url;
+  });
+  function MM_swapImage() { //v3.0
+    var i, j = 0, x, a = MM_swapImage.arguments; document.MM_sr = new Array; for (i = 0; i < (a.length - 2) ; i += 3)
+        if ((x = MM_findObj(a[i])) != null) { document.MM_sr[j++] = x; if (!x.oSrc) x.oSrc = x.src; x.src = a[i + 2]; }
+}
+
+function MM_swapImgRestore() { //v3.0
+    var i, x, a = document.MM_sr; for (i = 0; a && i < a.length && (x = a[i]) && x.oSrc; i++) x.src = x.oSrc;
+}
